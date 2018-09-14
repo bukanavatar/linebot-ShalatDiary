227634a38c691a95fafc617c658052417020f970
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2018-09-14 23:02:43 +0700

    Fix Template message

--- Events/events_follow.js
+++ Events/events_follow.js
@@ -38,8 +38,6 @@ export function follow(replyToken, source, client, db) {
             altText: 'Silahkan Share Lokasimu yang sekarang',
             template: {
                 type: 'buttons',
-                thumbnailImageUrl: 'https://images.unsplash.com/photo-1533615767566-273ffe95ed17?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=030e680a09fbe23f188b56248a6e30e6&auto=format&fit=crop&w=1027&q=80',
-                imageSize: 'cover',
                 title: 'Share Lokasimu',
                 actions: [
                     {
