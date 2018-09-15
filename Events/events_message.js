48754c74db9dc22c57bef8372ac678786fc32089
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2018-09-15 19:28:01 +0700

    Add default case

--- Events/events_message.js
+++ Events/events_message.js
@@ -301,6 +301,35 @@ export function handleText(message, replyToken, source, timestamp, client, db) {
                 case 'tidak shalat':
                     setTambahShalat(waktuShalat[3]);
                     break;
+                default:
+                    client.replyMessage(replyToken, {
+                        type: 'text',
+                        text: 'Sepertinya kamu butuh bantuan, silahkan pilih menu dibawah ini ya',
+                        quickReply: {
+                            items: [{
+                                type: "action",
+                                action: {
+                                    type: "message",
+                                    label: "Jadwal Shalat",
+                                    text: "Jadwal Shalat"
+                                }
+                            }, {
+                                type: "action",
+                                action: {
+                                    type: "message",
+                                    label: "Test",
+                                    text: "test"
+                                }
+                            }, {
+                                type: "action",
+                                action: {
+                                    type: "message",
+                                    label: "Tambah Record Shalat",
+                                    text: "tambah shalat"
+                                }
+                            }]
+                        }
+                    });
             }
         });
 
