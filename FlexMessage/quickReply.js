ea1d492334fc0197d4b1b32862a8921eb9dbb4f6
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2018-10-07 20:18:23 +0700

    Change Set flag

--- FlexMessage/quickReply.js
+++ FlexMessage/quickReply.js
@@ -2,7 +2,7 @@ export default function () {
     return {
         items: [{
             type: "action",
-            imageUrl: "https://firebasestorage.googleapis.com/v0/b/shalatdiary.appspot.com/o/JadwalIcon-01.png?alt=media&token=d46fa996-124e-4b4d-a86a-7a6c2ddb5575"
+            imageUrl: "https://firebasestorage.googleapis.com/v0/b/shalatdiary.appspot.com/o/JadwalIcon-01.png?alt=media&token=d46fa996-124e-4b4d-a86a-7a6c2ddb5575",
             action: {
                 type: "message",
                 label: "Jadwal Shalat",
